9e891c1050b530b030ca3e9f17e91de525e97279
Juakamayo <j.pizarro02@ufromail.cl>
2025-09-26 00:17:04 -0300

    Primera version del main terminado, sujeto a bastantes cambios

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        ManejadorConsola manejadorConsola = new ManejadorConsola();
+
+        APILeerCalendar apiLeerCalendar = new APILeerCalendar();
+        APIGemini apiGemini = new APIGemini();
+        APIEscribirCalendar apiEscribirCalendar = new APIEscribirCalendar();
+
+        Logica coordinadorDeLogica = new Logica(apiLeerCalendar, apiGemini, apiEscribirCalendar, manejadorConsola);
+
+
+        //Definimos los datos de la reunion
+        List<String> participantes = List.of("jefe@ejemplo.com", "trabajador1@ejemplo.com", "trabajador2@ejemplo.com");
+        String tituloReunion = "Reunion de Sincronizacion del Proyecto";
+        int duracionMinutos = 30;
+
+
+
+        coordinadorDeLogica.agendarReunion(participantes, tituloReunion, duracionMinutos);
+
+    }
+
+
 }
