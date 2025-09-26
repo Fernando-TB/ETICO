f9f43336717d01821cb02295d78753ed0de12467
Gonzex <g.fuentes11@ufromail.cl>
2025-09-26 00:23:09 -0300

    Cambio en pom.xml y primera version de apigemini

--- src/APIGemini.java
+++ src/APIGemini.java
@@ -1,13 +1,6 @@
-import com.google.cloud.gemini.v1beta.GeminiClient;
-import com.google.cloud.gemini.v1beta.GenerateContentResponse;
-import com.google.cloud.gemini.v1beta.Content;
-import com.google.cloud.gemini.v1beta.Part;
-import java.io.IOException;
-
 public class APIGemini {
 
     private final String apiKey;
-    private GeminiClient client;
 
     public APIGemini(String apiKey) {
         this.apiKey = "AIzaSyCbKVka07ge-NtmX9hg2ETRVgi1z5JpMq8";
@@ -15,8 +8,8 @@ public class APIGemini {
 
     public String getOptimalTime(String fullPrompt) {
         try {
-
             String simulatedResponse = "startTime,endTime\n2025-10-01T14:00:00,2025-10-01T15:00:00\n";
+            System.out.println("Respuesta simulada de Gemini: \n" + simulatedResponse);
             return simulatedResponse;
         } catch (Exception e) {
             System.err.println("Error al comunicarse con la API de Gemini: " + e.getMessage());
