d6e6a5f22488420ba5b139b276de11ab0ccebe24
Fernando-TB <f.torres17@ufromail.cl>
2025-11-15 19:13:08 -0300

    feat(modelo): agrega gestión de equipo y verificación de miembros en Jefe.
    Se incorpora una lista de trabajadores a cargo del Jefe y se extiende el
    constructor para incluir la contraseña y el equipo.
    
    Se agregan los métodos:
    - agregarMiembro(): añade un nuevo trabajador al equipo validando duplicados.
    - miembroEnEquipo(): verifica si un trabajador ya pertenece al equipo usando su correo.
    
    También se importan utilidades de Objects para comparación segura.

--- src/Jefe.java
+++ src/Jefe.java
@@ -1,9 +1,32 @@
 import java.util.List;
+import java.util.Objects;
 
 public class Jefe extends Trabajador{
 
-    public Jefe(int edad, String nombre, String correo, String rol, List<EventoCalendario> calendario) {
-        super(edad, nombre, correo, rol, calendario);
+    private List<Trabajador> equipo;
+
+    public Jefe(int edad, String nombre, String correo,String contra, String rol, List<EventoCalendario> calendario, List<Trabajador> equipo) {
+        super(edad, nombre, correo, contra, rol, calendario);
+
+        this.equipo = equipo;
+    }
+
+    public void agregarMiembro(Trabajador miembroNuevo){
+
+        if(miembroEnEquipo(miembroNuevo)){
+            System.out.println("Miembro ya en equipo");
+        }else {
+            this.equipo.add(miembroNuevo);
+        }
+    }
+
+    public boolean miembroEnEquipo(Trabajador miembro){
+        for(Trabajador persona : this.equipo){
+            if(Objects.equals(miembro.getCorreo(), persona.getCorreo())){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
